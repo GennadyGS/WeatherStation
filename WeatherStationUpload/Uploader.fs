15963194cf69da70b16b86f6af3f6b532ca4dc2f
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-07-22 16:56:14 +0300

    Add parsing

--- WeatherStationUpload/Uploader.fs
+++ WeatherStationUpload/Uploader.fs
@@ -1,7 +1,27 @@
 module WeatherStationUpload.Uploader
 
 open FSharp.Data
+open FSharp.Text.RegexProvider
+open System
+open System.Globalization
 
-type MobileAlertsMeasurements = HtmlProvider<"http://measurements.mobile-alerts.eu/Home/MeasurementDetails?deviceid=0301548CBC4A&vendorid=270f2261-3477-4872-9580-ead9cab3044c&appbundle=eu.mobile_alerts.mobilealerts">
+type 
+    Mesasurement = 
+        { TemperatureInside: decimal
+          TemperatureOutside: decimal
+          HimidityInside: decimal
+          HimidityOutside: decimal }
+type 
+    MobileAlertsMeasurements = HtmlProvider<"http://measurements.mobile-alerts.eu/Home/MeasurementDetails?deviceid=07523951F222&vendorid=270f2261-3477-4872-9580-ead9cab3044c&appbundle=eu.mobile_alerts.mobilealerts">
 
-let url = "http://measurements.mobile-alerts.eu/Home/MeasurementDetails"
+let url = "http://measurements.mobile-alerts.eu/Home/MeasurementDetails?deviceid=07523951F222&vendorid=270f2261-3477-4872-9580-ead9cab3044c&appbundle=eu.mobile_alerts.mobilealerts"
+
+type TemperatureRegex = Regex< @"(?<Temperature>\d+.\d+)\s*C" >
+
+let measurements = 
+        MobileAlertsMeasurements.Load(url).Tables.``Obolon Measurements``.Rows
+        |> Seq.map (fun row -> 
+            { TemperatureInside = Decimal.Parse(TemperatureRegex().TypedMatch(row.``Temperature Inside``).Temperature.Value, CultureInfo.InvariantCulture)
+              TemperatureOutside = Decimal.Parse(TemperatureRegex().TypedMatch(row.``Temperature Outside``).Temperature.Value, CultureInfo.InvariantCulture)
+              HimidityInside = row.``Humidity Inside``
+              HimidityOutside =row.``Humidity Outside``})
