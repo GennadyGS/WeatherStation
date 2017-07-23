49b22f84849ea305256e8368d93c78ba978bbf6d
GennadyGS <GennadyGS@gmail.com>
2017-07-23 21:30:51 +0300

    Parse with regular HTML parser

---
+++ WeatherStationUpload/Main.fs
@@ -0,0 +1,16 @@
+﻿module Main
+
+open FSharp.Data
+open WeatherStationUpload.Parser
+
+[<EntryPoint>]
+let main argv = 
+    let url = "http://measurements.mobile-alerts.eu/Home/MeasurementDetails?deviceid=07523951F222&vendorid=270f2261-3477-4872-9580-ead9cab3044c&appbundle=eu.mobile_alerts.mobilealerts"
+
+    HtmlDocument.Load(url) 
+    |> parseHtmlDocument 
+    |> Seq.toList
+    |> printf "%A"
+
+    0
+
