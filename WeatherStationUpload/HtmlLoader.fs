2ea6c336d09e9f907ad207c7a44b93da7a9a2adf
GennadyGS <GennadyGS@gmail.com>
2017-08-12 14:50:37 +0300

    Add HtmlLoader class

---
+++ WeatherStationUpload/HtmlLoader.fs
@@ -0,0 +1,8 @@
+﻿module WeatherStationUpload.HtmlLoader
+
+open FSharp.Data
+
+let loadHtml stationId = 
+    stationId
+    |> sprintf "http://measurements.mobile-alerts.eu/Home/MeasurementDetails?deviceid=%s&vendorid=270f2261-3477-4872-9580-ead9cab3044c&appbundle=eu.mobile_alerts.mobilealerts"
+    |> HtmlDocument.Load
--- WeatherStationUpload/Main.fs
+++ WeatherStationUpload/Main.fs
@@ -2,12 +2,14 @@
 
 open FSharp.Data
 open WeatherStationUpload.HtmlParser
+open WeatherStationUpload.HtmlLoader
+
+let stationId = "07523951F222"
 
 [<EntryPoint>]
 let main argv = 
-    let url = "http://measurements.mobile-alerts.eu/Home/MeasurementDetails?deviceid=07523951F222&vendorid=270f2261-3477-4872-9580-ead9cab3044c&appbundle=eu.mobile_alerts.mobilealerts"
-
-    HtmlDocument.Load(url) 
+    stationId
+    |> loadHtml
     |> parseHtmlDocument 
     |> Seq.toList
     |> printf "%A"
