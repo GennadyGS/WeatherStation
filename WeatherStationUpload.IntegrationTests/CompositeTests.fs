13131f321b5c72d34002c74b5e19d6159b62bab9
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-08-12 13:26:24 +0300

    Improve test

--- WeatherStationUpload.IntegrationTests/CompositeTests.fs
+++ WeatherStationUpload.IntegrationTests/CompositeTests.fs
@@ -1,8 +1,10 @@
 ﻿namespace WeatherStationUpload.IntegrationTests
 
 open Xunit
+open WeatherStationUpload.Composite
 
 type CompositeTests() = 
     [<Fact>]
     member this.``Test`` () = 
+        loadWeatherData (System.DateTime.Now.AddHours(-1.0)) System.DateTime.Now 
         ()
