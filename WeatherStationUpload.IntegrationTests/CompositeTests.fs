0b17a758f5023ae3eb145b4880f6b1f7e2fe8987
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-08-13 21:06:16 +0300

    Fix integration test

--- WeatherStationUpload.IntegrationTests/CompositeTests.fs
+++ WeatherStationUpload.IntegrationTests/CompositeTests.fs
@@ -8,8 +8,8 @@ type CompositeTests() =
     let stationId = "07523951F222"
     
     [<Fact>]
-    member this.``Test`` () = 
+    member this.``ProcessWeatherData should return non-empty result for the last day`` () = 
         let dateFrom = System.DateTime.Now.AddDays(-1.0)
         let dateTo = System.DateTime.Now
         let result = processWeatherData dateFrom dateTo stationId
-        ()
+        Assert.NotEmpty result
