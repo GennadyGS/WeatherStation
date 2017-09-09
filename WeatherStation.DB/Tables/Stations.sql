1ea1bdae580bca06e61bb0409147590fad0b90d7
GennadyGS <GennadyGS@gmail.com>
2017-09-09 16:04:55 +0300

    Add database project

---
+++ WeatherStation.DB/Tables/Stations.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Stations]
+(
+	[VendorId] UNIQUEIDENTIFIER NOT NULL , 
+    [DeviceId] VARCHAR(16) NOT NULL, 
+    [Name] VARCHAR(256) NOT NULL, 
+    [Description] VARCHAR(4096) NULL, 
+    PRIMARY KEY ([VendorId], [DeviceId])
+)
