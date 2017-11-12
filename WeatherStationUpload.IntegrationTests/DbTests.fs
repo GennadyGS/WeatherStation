c5807316b34c1bf44c5d85574db8c29fee4eb687
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-11-12 20:13:30 +0200

    Add DbTests module

---
+++ WeatherStationUpload.IntegrationTests/DbTests.fs
@@ -0,0 +1,22 @@
+﻿namespace WeatherStationUpload.IntegrationTests
+
+open FSharp.Data.Sql.Providers
+open System.Configuration
+
+type DbTests () =
+    let executeSqlCommand sql = 
+        using 
+            (MSSqlServer.createConnection ConfigurationManager.ConnectionStrings.["WeatherStation"].ConnectionString)
+            (fun connection -> 
+                connection.Open()
+                let command = MSSqlServer.createCommand sql connection
+                command.ExecuteNonQuery())
+    
+    do
+        try
+            ["Observations"]
+            |> List.map ((sprintf "TRUNCATE TABLE %s") >> executeSqlCommand)
+            |> ignore
+        with
+          | :? System.Data.SqlClient.SqlException as e -> 
+            System.Console.Error.WriteLine(sprintf "Error cleaning databases: %s" (e.ToString()))    
