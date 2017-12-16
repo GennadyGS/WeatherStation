3581bf67c0c9716d82fa85914ea45481db44f753
GennadyGS <GennadyGS@gmail.com>
2017-12-16 12:38:14 +0200

    Async draft

--- WeatherStationUpload/DataUploader.fs
+++ WeatherStationUpload/DataUploader.fs
@@ -11,4 +11,13 @@ let uploadData
     |> List.map (fun measurement -> stationId, measurement)
     |> DbService.insertMeasurements connectionString
 
+let uploadDataAsync
+        (connectionString : string)
+        (timeInterval : TimeInterval)
+        (deviceInfo: DeviceInfo)
+        (stationId: StationId): Async<unit> =
+    collectDataAsync timeInterval deviceInfo
+    |> AsyncUtils.map (List.map (fun measurement -> stationId, measurement))
+    |> AsyncUtils.bind (DbService.insertMeasurementsAsync connectionString)
+
     
