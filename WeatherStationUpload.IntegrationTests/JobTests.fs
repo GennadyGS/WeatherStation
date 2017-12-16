13c1e86b1fb1ceb1b10cfaf7289c9386c3473bc2
GennadyGS <GennadyGS@gmail.com>
2017-12-16 15:52:20 +0200

    Migrate JobTests to asynchronous

--- WeatherStationUpload.IntegrationTests/JobTests.fs
+++ WeatherStationUpload.IntegrationTests/JobTests.fs
@@ -13,14 +13,17 @@ type JobTests() =
 
     [<Fact>]
     let ``Execute should load data incrementally`` () =
-        let maxTimeInterval = TimeSpan.FromDays(3.0)
+        async {
+            let maxTimeInterval = TimeSpan.FromDays(3.0)
     
-        Job.execute testConnectionString (testTime.AddDays(-1.0)) maxTimeInterval
+            do! Job.executeAsync testConnectionString (testTime.AddDays(-1.0)) maxTimeInterval
 
-        let result1 = DbService.getMeasurements testConnectionString
-        Assert.True (result1.Length > 250)
+            let result1 = DbService.getMeasurements testConnectionString
+            Assert.True (result1.Length > 250)
 
-        Job.execute testConnectionString testTime maxTimeInterval
+            do! Job.executeAsync testConnectionString testTime maxTimeInterval
 
-        let result2 = DbService.getMeasurements testConnectionString
-        result2.Length |> should be (greaterThan result1.Length)
+            let result2 = DbService.getMeasurements testConnectionString
+            result2.Length |> should be (greaterThan result1.Length)
+        }
+        |> Async.RunSynchronously
