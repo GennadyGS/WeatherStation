611c098f251f66b2087efaa20c05de58a6702d1d
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-12-30 17:27:12 +0200

    Add console application

---
+++ WeatherStationUploader/Program.fs
@@ -0,0 +1,20 @@
+﻿open WeatherStationUpload
+open Serilog
+open FSharp.Configuration
+open System
+
+type Settings = AppSettings<"app.config">
+
+[<EntryPoint>]
+let main argv = 
+    let logger =
+        LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+    Job.executeAsync 
+        logger 
+        Settings.ConnectionStrings.WeatherStation
+        DateTime.Now
+        (TimeSpan.FromDays(Settings.MaxTimeIntervalDays |> float))
+    |> Async.RunSynchronously
+    0 // return an integer exit code
