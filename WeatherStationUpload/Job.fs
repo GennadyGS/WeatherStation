573bf61441a7030cbc7b5286038a560d89d4baa9
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-12-31 21:56:54 +0200

    Improve error handling

--- WeatherStationUpload/Job.fs
+++ WeatherStationUpload/Job.fs
@@ -12,17 +12,19 @@ let executeAsync
         let getIntervalStartTime = function
             | Some (time: DateTime) -> time + TimeSpan.FromSeconds(1.0)
             | None -> intervalEndTime.Add(-maxTimeInterval)
-        try
-            DataUploader.uploadDataAsync
-                logger
-                connectionString
-                { From = (getIntervalStartTime lastMeasurementTime)
-                  To = intervalEndTime }
-                deviceInfo
-                stationId
-        with
-        | _ as e -> 
-            logger.Error(e, "Error uploading data for device {device}", deviceInfo.DeviceId) |> async.Return
+        async {
+            try
+                return! DataUploader.uploadDataAsync
+                    logger
+                    connectionString
+                    { From = (getIntervalStartTime lastMeasurementTime)
+                      To = intervalEndTime }
+                    deviceInfo
+                    stationId
+            with
+            | _ as e -> 
+                logger.Error(e, "Error uploading data for device {device}", deviceInfo.DeviceId)
+        }
 
     logger.Information("Start job; intervalEndTime: {intervalEndTime}; maxTimeInterval: {maxTimeInterval}", intervalEndTime, maxTimeInterval)
     DbService.getStationsLastMeasurementsAsync logger connectionString
