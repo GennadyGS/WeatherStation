7b442eca1aa6847f0429577af627015168b05b52
GennadyGS <GennadyGS@gmail.com>
2017-12-31 21:17:07 +0200

    Bypass inserting empty data to database

--- WeatherStationUpload/DataUploader.fs
+++ WeatherStationUpload/DataUploader.fs
@@ -12,8 +12,10 @@ let uploadDataAsync
     logger.Information("Upload data for device {device} from {from} to {to}", deviceInfo.DeviceId, timeInterval.From, timeInterval.To)
     collectDataAsync logger timeInterval deviceInfo
     |> AsyncUtils.map (List.map (fun measurement -> stationId, measurement))
-    |> AsyncUtils.combineWithAndInore (fun records -> logger.Information("Inserting {measurementCount} measurements in database", records.Length))
-    |> AsyncUtils.bind (DbService.insertMeasurementsAsync logger connectionString)
-    |> AsyncUtils.combineWithAndInore (fun _ -> logger.Information("Upload data for station {stationId} complete", stationId))
+    |> AsyncUtils.bind 
+        (function 
+        | [] -> logger.Information("No new data to insert for device {device}", deviceInfo.DeviceId) |> async.Return
+        | measurements -> DbService.insertMeasurementsAsync logger connectionString measurements)
+    |> AsyncUtils.combineWithAndInore (fun _ -> logger.Information("Upload data for device {device} complete", deviceInfo.DeviceId))
 
     
