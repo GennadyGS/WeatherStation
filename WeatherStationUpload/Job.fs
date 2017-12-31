d42572a0db3faf7a473e32ae72816383ba71db2f
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-12-31 21:29:51 +0200

    Refactoring: extract function uploadDataForDeviceAsync

--- WeatherStationUpload/Job.fs
+++ WeatherStationUpload/Job.fs
@@ -8,25 +8,25 @@ let executeAsync
         (connectionString: string) 
         (intervalEndTime: DateTime)
         (maxTimeInterval: TimeSpan): Async<unit> = 
+    let uploadDataForDeviceAsync (stationId, deviceInfo, lastMeasurementTime) = 
+        let getIntervalStartTime = function
+            | Some (time: DateTime) -> time + TimeSpan.FromSeconds(1.0)
+            | None -> intervalEndTime.Add(-maxTimeInterval)
+        try
+            DataUploader.uploadDataAsync
+                logger
+                connectionString
+                { From = (getIntervalStartTime lastMeasurementTime)
+                  To = intervalEndTime }
+                deviceInfo
+                stationId
+        with
+        | _ as e -> 
+            logger.Error(e, "Error uploading data for device {device}", deviceInfo.DeviceId) |> async.Return
+
     logger.Information("Start job; intervalEndTime: {intervalEndTime}; maxTimeInterval: {maxTimeInterval}", intervalEndTime, maxTimeInterval)
-    let getIntervalStartTime = function
-        | Some (time: DateTime) -> time + TimeSpan.FromSeconds(1.0)
-        | None -> intervalEndTime.Add(-maxTimeInterval)
     DbService.getStationsLastMeasurementsAsync logger connectionString
     |> AsyncUtils.bind(
-        List.map 
-            (fun (stationId, deviceInfo, lastMeasurementTime) -> 
-                try
-                    DataUploader.uploadDataAsync
-                        logger
-                        connectionString
-                        { From = (getIntervalStartTime lastMeasurementTime)
-                          To = intervalEndTime }
-                        deviceInfo
-                        stationId
-                with
-                | _ as e -> 
-                    logger.Error(e, sprintf "Error uploading data from station %A" stationId) 
-                    |> async.Return )
+        List.map uploadDataForDeviceAsync
         >> AsyncUtils.runSequentially)
     |> AsyncUtils.combineWithAndInore (fun _ -> logger.Information("Job complete"))
