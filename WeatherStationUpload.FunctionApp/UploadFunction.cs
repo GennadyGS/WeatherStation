6a3da2ada166868e64c0d5f76ed637cdc1302a08
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-02-17 12:15:42 +0200

    Rename function to UploadFunction

--- WeatherStationUpload.FunctionApp/UploadFunction.cs
+++ WeatherStationUpload.FunctionApp/UploadFunction.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Configuration;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -8,25 +7,24 @@ using Serilog.Sinks.AzureWebJobsTraceWriter;
 
 namespace WeatherStationUpload.FunctionApp
 {
-    public static class WeatherStationUpload
+    public static class UploadFunction
     {
-        [FunctionName("WeatherStationUpload")]
+        [FunctionName(nameof(UploadFunction))]
         public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter traceWriter)
         {
             ILogger logger = new LoggerConfiguration()
                 .WriteTo.TraceWriter(traceWriter)
                 .CreateLogger();
-            logger.Information($"WeatherStationUpload function started at UTC time: {DateTime.UtcNow}");
+            logger.Information($"WeatherStationUpload function started");
 
-            var maxTimeInterval = TimeSpan.FromDays(100);
-
-            var result = await JobAdapter.executeAsync(logger,
+            await JobAdapter.executeAsync(logger,
                 connectionString: ConfigurationReader.DbConnectionString,
                 dbInsertTimeout: ConfigurationReader.DbInsertTimeout,
                 dbInsertBatchSize: ConfigurationReader.DbInsertBatchSize,
                 intervalEndTimeUtc: DateTime.UtcNow,
                 maxTimeInterval: ConfigurationReader.MaxTimeInterval);
-            logger.Information($"WeatherStationUpload function finished with result {result} at UTC time: {DateTime.UtcNow}");
+
+            logger.Information($"WeatherStationUpload function finished");
         }
     }
 }
